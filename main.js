f7d0c214b4be8f9e38715a784d4d562bf002e3bc
omaroium <omaralimycan@gmail.com>
2022-07-25 16:07:44 +0300

    best code ever seen

---
+++ main.js
@@ -0,0 +1,9 @@
+function changebgcolor(color) {
+	if(document.body.style.backgroundColor== color){
+		document.body.style.backgroundColor =  "blue";
+	} 
+	else{
+	document.body.style.backgroundColor =  color;
+}
+	
+}
